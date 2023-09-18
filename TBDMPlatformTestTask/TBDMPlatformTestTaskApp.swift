75ab877c236bef9219f9afd2a7ebc5bb315d6957
Boris Kravchenko <115030529+allmycalifornia@users.noreply.github.com>
2023-09-18 09:42:36 +0300

    Initial Commit

---
+++ TBDMPlatformTestTask/TBDMPlatformTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TBDMPlatformTestTaskApp.swift
+//  TBDMPlatformTestTask
+//
+//  Created by Борис Кравченко on 18.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct TBDMPlatformTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
